dcb4c192fffa67c6ef35a073022d2193f039b77b
AjayAstro <81108959+AjayAstro@users.noreply.github.com>
2022-03-01 17:49:28 -0500

    Update FirstCommit.java

--- src/gitHubRepo/FirstCommit.java
+++ src/gitHubRepo/FirstCommit.java
@@ -4,22 +4,8 @@ public class FirstCommit {
 	
 	public static void main(String[] args) {
 		
-		
-		
-		System.out.println("This Is another fist commit 02/27/2022");
-		System.out.println("This is me trying to commit again");
-		
 
-
-		System.out.println("I am now another team member commiting my work");
-		System.out.println("This is another team member");
-		
-		System.out.println("6:36");
-
-		
-		
-		
-		System.out.println("Team innocent commit");
+		System.out.println("this is in the master branch");
 		
 	}
 
